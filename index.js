30dd0ae1d822b9bcc208c838b51304f6b4de86b0
Salah-eddine Etangi <mo.momen1438@gmail.com>
2022-03-15 20:48:45 +0100

    whole phonebook

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+morgan.token('data-shower', (request, response) => JSON.stringify(request.body))
+
+const app = express()
+
+app.use(express.json())
+app.use(morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      tokens['data-shower'](req, res)
+    ].join(' ')
+  }))
+app.use(cors())
+app.use(express.static('build'))
+
+let persons = [
+    { 
+        id: 1,
+        name: "Arto Hellas", 
+        number: "040-123456"
+    },
+    { 
+        id: 2,
+        name: "Ada Lovelace", 
+        number: "39-44-5323523"
+    },
+    { 
+        id: 3,
+        name: "Dan Abramov", 
+        number: "12-43-234345"
+    },
+    { 
+        id: 4,
+        name: "Mary Poppendieck", 
+        number: "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    const currentDate = new Date()
+    response.send(`<p>Phonebook has info for ${persons.length} people</p>
+                   <p>${currentDate}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if(person){
+        response.json(person)
+    }
+    else{
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+const generateId = () => Math.floor(Math.random() * 1000000)
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if(!body.name || !body.number){
+        return response.status(400).json({
+            error: 'name or number is missing'
+        })
+    }
+    if(persons.find(person => person.name === body.name)){
+        return response.status(400).json({
+            error: 'the name already exists in the phonebook'
+        })
+    }
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(person)
+    response.json(person)
+})
+
+app.listen(3001)
+
